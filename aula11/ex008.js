62ab2c9e94a34251a86ba498e41b2c26a5727f65
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-06-04 18:18:19 -0300

    atualização

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
 var vel = 60.5
 console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, foi multado!')
+}
+console.log('Dirija sempre usando cinto de segurança')
