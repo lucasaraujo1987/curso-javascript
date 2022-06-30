88b2a5f161814bf1998770c374e135ea8f7bdf9f
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-06-30 14:17:35 -0300

    Update ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5,8,2,9,3]
+num.push(1) //acrescenta um número no final
+num.sort() //põe os vetores em ordem númerica
+console.log(num) //exibe os vetores
+console.log(`O vetor tem ${num.length} posições`) //length mostra a quantidade de posições
+console.log(`O primeiro valor do vetor é ${num[0]}`) //[] exibe a posição do vetor
