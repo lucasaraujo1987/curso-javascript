d9ff955df712cce86f84926e1d10099da1a26021
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-06-30 14:33:23 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,3 +4,9 @@ num.sort() //põe os vetores em ordem númerica
 console.log(num) //exibe os vetores
 console.log(`O vetor tem ${num.length} posições`) //length mostra a quantidade de posições
 console.log(`O primeiro valor do vetor é ${num[0]}`) //[] exibe a posição do vetor
+let pos = num.indexOf(4)
+if(pos == -1){
+    console.log('O valor 8 não foi encontrado')
+} else{
+    console.log(`O valor 8 está na posição ${pos}`)
+}
